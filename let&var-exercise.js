b3519c518661b57e0bbbe0a5e1546ff33a568370
Ibrahimipaye <119717430+Ibrahimipaye@users.noreply.github.com>
2023-01-29 21:44:56 -0500

    Add files via upload

---
+++ let&var-exercise.js
@@ -0,0 +1,21 @@
+// What is the difference between var and let?
+You can redeclare and reassign values with 'var' 
+while you can only  reassigned but not redeclared 'let'
+
+// What is the difference between var and const?
+
+You can redeclare and reassign values with 'var' 
+while  'const' keyword prevents a variable from ever being reassigned or redeclared.
+
+// What is the difference between let and const?
+
+you can only  reassigned but not redeclared 'let'
+while  'const' keyword prevents a variable from ever being reassigned or redeclared.
+
+// What is hoisting?
+
+
+In JavaScript, Hoisting is a kind of default behavior in which all the declarations 
+either variable declaration or function declaration are moved at the top of the scope just before executing the program's code.
+ However, it can be considered an advantage because all functions and variable declarations are placed to the top of their scope no matter where
+  they are all declared anywhere in the whole program, even regardless of whether they are declared global or local
