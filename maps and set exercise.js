49291e1630cf9362b18be66b5e7dfc1496d41af2
Ibrahimipaye <119717430+Ibrahimipaye@users.noreply.github.com>
2023-01-30 21:12:39 -0500

    Add files via upload

---
+++ maps and set exercise.js
@@ -0,0 +1,54 @@
+new Set([1,1,2,2,3,4])
+// {1,2,3,4}
+
+// hr 
+[...new Set("referee")].join("")
+
+"ref"
+
+// hr 
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+// 0: {Array(3) => true}
+// 1: {Array(3) => false}
+
+// hr 
+
+hasDuplicate
+const hasDuplicate = arr => new Set(arr).size !== arr.length;
+
+// hr 
+
+// countVowel(str) {
+//      // find the count of vowels 
+//      const count = str.match (/ [aeiou]/gi).length; 
+//      // return number of vowels 
+//      return count; 
+//     } 
+//     // take input 
+//     const string = prompt ('Enter a string: ');
+//      const result = countVowel (string); 
+//      console.log (result);
+//      vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+// vowelCount('Colt') // Map { 'o' => 1 }
+
+// function isVowel(char){
+//   return "aeiou".includes(char);
+// }
+
+function vowelCount(str){
+  const vowelMap = new Map();
+  for(let char of str){
+    let lowerCaseChar = char.toLowerCase()
+    if(isVowel(lowerCaseChar)){
+      if(vowelMap.has(lowerCaseChar)){
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      } else {
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
